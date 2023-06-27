e382794cccffb9256f3c706b9f86ed32fb317b5a
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-27 11:17:40 +0300

    Course project data binding

---
+++ 06. Course Project Components Databinding/angular-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg?crop=0.6666666666666667xw:1xh;center,top&resize=1200:*'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg?crop=0.6666666666666667xw:1xh;center,top&resize=1200:*'
+    ),
+  ];
+}
