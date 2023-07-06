52066a83c466d1d4565636e07f986c5658db0476
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-06 18:09:40 +0300

    Controlling Navigation with canDeactivate

--- 11. Changing Pages with Routing/routing-start/src/app/app.module.ts
+++ 11. Changing Pages with Routing/routing-start/src/app/app.module.ts
@@ -9,10 +9,14 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+
 import { ServersService } from './servers/servers.service';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,7 @@ import { AuthGuard } from './auth-guard.service';
     ServerComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService, AuthService, AuthGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
