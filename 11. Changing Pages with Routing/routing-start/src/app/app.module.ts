6900d8c64c3cdabec2b4f841ac034c958cb3e787
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-06 17:08:27 +0300

    Protecting Routes with canActivate

--- 11. Changing Pages with Routing/routing-start/src/app/app.module.ts
+++ 11. Changing Pages with Routing/routing-start/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
     ServerComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
