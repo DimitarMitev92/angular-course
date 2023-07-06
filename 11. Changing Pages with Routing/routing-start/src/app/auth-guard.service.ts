84a57c5d422c6974b02654e489e7a7cdfe6e3459
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-06 17:12:51 +0300

    Protecting  Nested Routes with canActivateChild

--- 11. Changing Pages with Routing/routing-start/src/app/auth-guard.service.ts
+++ 11. Changing Pages with Routing/routing-start/src/app/auth-guard.service.ts
@@ -1,15 +1,17 @@
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
+  UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,5 +24,16 @@ export class AuthGuard implements CanActivate {
         this.router.navigate(['/']);
       }
     });
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
   }
 }
